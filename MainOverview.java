b8eb8d5864b9fde718bfff3c9a5ec7c66199dda3
yajan-singh <yajan08@gmail.com>
2020-05-02 18:16:51 -0400

    test add of real file

--- MainOverview.java
+++ MainOverview.java
@@ -1,3 +1,15 @@
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class MainOverview {
-    int x = 1;
+    public MainOverview(){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(null);
+    }
+    public static void main(String[] args) {
+        new MainOverview();
+    }
 }
