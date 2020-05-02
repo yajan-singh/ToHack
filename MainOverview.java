8305675e779ffb9003b576e4c113e7210e1ebfbb
yajan-singh <yajan08@gmail.com>
2020-05-02 19:19:35 -0400

    deleted

--- MainOverview.java
+++ MainOverview.java
@@ -1,16 +1,35 @@
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import java.util.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class MainOverview extends JFrame {
+
+    JPanel p = new JPanel();
+    JButton b = new JButton("hello");
+    JTextField t = new JTextField("Hi", 20);
+    JTextArea ta = new JTextArea("How does this look?", 2, 20);
+    JLabel l = new JLabel("Lable");
+    public static void main(String[] args) {
+        new MainOverview();
+
+    }
+
     public MainOverview(){
         super("GUI");
         setSize(400,300);
         setResizable(true);
-        setDefaultCloseOperation();
-    }
-    public static void main(String[] args) {
-        new MainOverview();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        p.add(b);
+        p.add(t);
+        p.add(ta);
+        p.add(l);
+        add(p);
 
+        setVisible(true);
     }
 }
