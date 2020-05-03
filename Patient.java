102f497f84ef723ec53c7b413d2e745077eb9ee1
ferrisdavid <59101892+ferrisdavid@users.noreply.github.com>
2020-05-02 20:01:43 -0400

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,51 +1,17 @@
-import java.util.ArrayList;
+ import java.util.ArrayList;
 import java.io.*;
 
 public class Patient 
 {
+    
     String name;
-    int age;
-    ArrayList<HospitolVisits> hospitolVisits;
-
-    public Patient() 
-    {
-        
-    }
-}
-
-public void readFile(Scanner scanner) throws BadFileFormatException 
-{
- 
-    try 
-    {
-        while (scanner.hasNext()) 
-        {
-                
-                String patientName = scanner.next();
-                String id = scanner.next();
-                    
-                if (isNumeric(id) == false || isStringOnlyAlphabet(studentName) == false || id.isEmpty() || studentName.isEmpty()) //if id contains letters, if studetn name contains numbers and then check the key and the element if they are empty 
-                {
-                    throw new BadFileFormatException();
-
-                }
-                Student s = new Student(studentName,id);
-                students.put(id,s);
+    int age; 
     
-        }
-    }
-    
-    catch (NoSuchElementException e)
+
+    public Patient(String name,int age) 
     {
-        throw new BadFileFormatException();
+        this.name = name;
+        this.age = age;
     }
-         
 }
 
-}
-
-class BadFileFormatException extends IOException
-{
-	public BadFileFormatException() {}
-
-}
