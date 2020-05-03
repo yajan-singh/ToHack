61f6b2cba58433c4bed32978e31b4f91a784cef6
ferrisdavid <59101892+ferrisdavid@users.noreply.github.com>
2020-05-02 20:04:42 -0400

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -4,15 +4,29 @@ import java.io.*;
 public class Patient 
 {
     
-    String name;
-    int age; 
-    
+    private String name;
+    private int age; 
+    private String condition;
 
-    public Patient(String name,int age) 
+    public Patient(String name,int age,String condition) 
     {
         this.name = name;
         this.age = age;
+        this.condition = condition;
     }
+    public String getName()
+    {
+        return name;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public String getCondition()
+    {
+        return condition;
+    }
+    
 }
 
 
